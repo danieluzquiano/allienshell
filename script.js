a2d2360839fa6662729dc5cd793cc35967dbea48
danieluzquiano <55982609+danieluzquiano@users.noreply.github.com>
2022-12-27 13:38:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-t=1e4
+t=1e5,draw=_=>{for(createCanvas(w=windowWidth,h=windowHeight),background(0),translate(w/2,h/2),scale(225),noStroke(),fill(255),n=map(mouseX,0,w,0,5),m=map(mouseY,0,h,0,10),a=0;a<TWO_PI;a+=4e-4)x=1e4*a,y=cos(a+t)/sin(x),e=cos(x)%a,f=2*noise(tan(e)),circle(sin(tan(cos(y))+n)*f*e,sin(tan(sin(y))+m)*f,.0091);t-=.003}//#つぶやきProcessing
+/*t=1e4
 setup=_=>{createCanvas(w=windowWidth,h=windowHeight),noiseSeed(fxrand()*999999)}
   draw=_=>{
     for(
@@ -20,3 +21,4 @@ setup=_=>{createCanvas(w=windowWidth,h=windowHeight),noiseSeed(fxrand()*999999)}
     circle(sin(tan(cos(y))+n)*f*e,sin(tan(sin(y))+m)*f,.012);
     t += .03
   }; //#つぶやきProcessing
+  */
