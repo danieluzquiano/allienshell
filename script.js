27bee5f9143b1802a696b2310de245a3fb010c63
danieluzquiano <55982609+danieluzquiano@users.noreply.github.com>
2022-12-31 10:26:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //t=1e5,draw=_=>{for(createCanvas(w=windowWidth,h=windowHeight),background(0),translate(w/2,h/2),scale(225),noStroke(),fill(255),n=map(mouseX,0,w,0,5),m=map(mouseY,0,h,0,10),a=0;a<TWO_PI;a+=4e-4)x=1e4*a,y=cos(a+t)/sin(x),e=cos(x)%a,f=2*noise(tan(e)),circle(sin(tan(cos(y))+n)*f*e,sin(tan(sin(y))+m)*f,.0091);t-=.003}//#つぶやきProcessing
-t=1e4
+t=1e4,s=0
 setup=_=>{createCanvas(w=windowWidth,h=windowHeight),noiseSeed(fxrand()*999999)}
   draw=_=>{
     for(
@@ -18,6 +18,7 @@ setup=_=>{createCanvas(w=windowWidth,h=windowHeight),noiseSeed(fxrand()*999999)}
     y=cos(a)/sin(t/x),
     e=cos(x)%a,
     f=2.5*noise(tan(e)),
-    circle(sin(tan(cos(y))+n)*f*e,sin(tan(sin(y))+t*0.1+m)*f,.012);
-    t += .03
+    circle(sin(tan(cos(y))+n)*f*e,sin(tan(sin(y))+s+m)*f,.012);
+    t += .03,
+    s+=.003
   }; //#つぶやきProcessing
